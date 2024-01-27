cd5fef43f230369adfdf63d7f5a22d1857865039
Soojungkim <soojung.kimm@gmail.com>
2024-01-27 10:38:05 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+//multiply
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2 
 
-// Write your code here
+//random
+let random = Math.floor(Math.random()*10) + 1;
+
+//mod
+const num3 = 22;
+const num4 = 9;
+let mod = num3 % num4;
+
+//max
+let max = Math.max(1, 5, 10, 15, 20);
